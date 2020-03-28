11d782a1e10b2f7a4797284eaf3f3b26d7fc1337
new <aji@gmail.com>
2020-03-28 11:11:23 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -11,37 +11,38 @@
 |
 */
 
+Route::group(['middleware' => ['forceSSL']], function () {
+  /*Route::get('/', 'HomeController@coming');*/
+  Route::post('/subscribe', 'HomeController@subscribe');
 
-/*Route::get('/', 'HomeController@coming');*/
-Route::post('/subscribe', 'HomeController@subscribe');
+  //Route::post('/users/loginas', 'HomeController@loginAs');
+  //Route::get('/loginas', 'HomeController@login');
 
-//Route::post('/users/loginas', 'HomeController@loginAs');
-//Route::get('/loginas', 'HomeController@login');
+  Auth::routes();
 
-Auth::routes();
-
-Route::get('/', 'HomeController@index');
-Route::get('/about', 'HomeController@about');
-Route::get('/contact', 'HomeController@contact');
-Route::post('/contact', 'HomeController@contact_');
-Route::get('/logout', 'HomeController@logout');
-Route::get('/terms', 'HomeController@terms');
-Route::get('/faq', 'HomeController@faq');
+  Route::get('/', 'HomeController@index');
+  Route::get('/about', 'HomeController@about');
+  Route::get('/contact', 'HomeController@contact');
+  Route::post('/contact', 'HomeController@contact_');
+  Route::get('/logout', 'HomeController@logout');
+  Route::get('/terms', 'HomeController@terms');
+  Route::get('/faq', 'HomeController@faq');
 
 
 
-Route::group(['middleware' => ['user', 'auth']], function(){ 
-  Route::post('/trado_shirt', 'HomeController@shirt');
-  Route::get('/dashboard', 'HomeController@dashboard');
-  Route::get('/farmshop', 'HomeController@investment');
-  Route::get('/edit', 'HomeController@edit');
-  Route::post('/edit_image', 'HomeController@edit_image');
-  Route::post('/edit_info', 'HomeController@edit_info');
-  Route::get('/password', 'HomeController@password');  
-  Route::post('/password', 'HomeController@edit_password');
-  Route::get('/activity', 'HomeController@activity');
-  Route::get('/shop_{id}', 'HomeController@investment_');
-  Route::get('/pay', 'PaymentController@index');
-  Route::post('/pay', 'PaymentController@redirectToGateway')->name('pay');
-  Route::get('/payment_callback', 'PaymentController@handleGatewayCallback');
+  Route::group(['middleware' => ['user', 'auth']], function () {
+    Route::post('/trado_shirt', 'HomeController@shirt');
+    Route::get('/dashboard', 'HomeController@dashboard');
+    Route::get('/farmshop', 'HomeController@investment');
+    Route::get('/edit', 'HomeController@edit');
+    Route::post('/edit_image', 'HomeController@edit_image');
+    Route::post('/edit_info', 'HomeController@edit_info');
+    Route::get('/password', 'HomeController@password');
+    Route::post('/password', 'HomeController@edit_password');
+    Route::get('/activity', 'HomeController@activity');
+    Route::get('/shop_{id}', 'HomeController@investment_');
+    Route::get('/pay', 'PaymentController@index');
+    Route::post('/pay', 'PaymentController@redirectToGateway')->name('pay');
+    Route::get('/payment_callback', 'PaymentController@handleGatewayCallback');
+  });
 });
